55090b9d098d90da1530a6ebd6708f2cb11a333f
Chanwit Kaewkasi <chanwit@gmail.com>
2021-12-25 18:53:36 +0700

    add main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+}
+
+variable "subject" {
+   type = string
+   default = "World"
+   description = "Subject to hello"
+}
+
+output "hello_world" {
+  value = "Hello, ${var.subject}!"
+}
