7353dc3abb7db80acb1afc915686490fbb8ac714
Chanwit Kaewkasi <chanwit@gmail.com>
2023-06-27 18:04:57 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
+variable "secret_subject" {
+    type = string
+    sensitive = true
+    description = "Secret subject"
+}
+
 variable "subject" {
    type = string
    default = "World"
@@ -9,5 +15,5 @@ variable "subject" {
 }
 
 output "hello_world" {
-  value = "Hello Plan plan, ${var.subject}!"
+  value = "Hello Plan plan, ${var.subject}! (secret: ${var.secret_subject})"
 }
