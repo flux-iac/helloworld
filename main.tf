083e8e9b8fb0d3909537bae6b0afef4e62e61cc9
Chanwit Kaewkasi <chanwit@gmail.com>
2023-06-28 14:07:31 +0700

    Revert "Update main.tf"
    This reverts commit 62fdacab0f85e7af08558d9c9f8cd03467dd36ee.
    This reverts commit 03e2651170cb9f30e6582ca3688b1ded37fda9aa.
    This reverts commit b68b3d86ef7acb5b164691e752aa46a5c732999c.
    This reverts commit 7353dc3abb7db80acb1afc915686490fbb8ac714.

--- main.tf
+++ main.tf
@@ -2,18 +2,12 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
-variable "secret_subject" {
-    type = string
-    sensitive = true
-    description = "Secret subject"
-}
-
 variable "subject" {
-   type = list
+   type = string
+   default = "World"
    description = "Subject to hello"
 }
 
 output "hello_world" {
-  sensitive = true
-  value = "Hello, ${var.subject}! (secret: ${var.secret_subject})"
+  value = "Hello Plan plan, ${var.subject}!"
 }
